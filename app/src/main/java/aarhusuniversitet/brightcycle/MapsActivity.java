79b6d93a3fad2c71578f97ec315a93685b1b2b5d
janekkkk <janekozga1@gmail.com>
2016-11-17 17:44:43 +0100

    Refactoring

--- app/src/main/java/aarhusuniversitet/brightcycle/MapsActivity.java
+++ app/src/main/java/aarhusuniversitet/brightcycle/MapsActivity.java
@@ -14,7 +14,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
@@ -60,26 +59,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mLocationProvider = new LocationProvider(this, this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        mMap.setPadding(0, 0, 0, 0);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        }
-
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        setUpMap();
     }
 
     @Override
@@ -94,7 +78,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onPause();
         mLocationProvider.disconnect();
     }
-    
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -105,14 +89,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
     private void setUpMap() {
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setPadding(0, 0, 0, 0);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        // Set markers etc
     }
 
     public void handleNewLocation(Location location) {
@@ -122,11 +106,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         double currentLongitude = location.getLongitude();
         LatLng latLng = new LatLng(currentLatitude, currentLongitude);
 
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
-        MarkerOptions options = new MarkerOptions()
-                .position(latLng)
-                .title("I am here!");
-        mMap.addMarker(options);
+        mMap.setMinZoomPreference(13.0f);
+        mMap.setMaxZoomPreference(18.0f);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 
