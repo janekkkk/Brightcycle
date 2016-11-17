584aa71174c22f62c5a9fb1b6d426b6c0cc7e12b
janekkkk <janekozga1@gmail.com>
2016-11-17 17:36:55 +0100

    Added getting current location long + lat

--- app/src/main/java/aarhusuniversitet/brightcycle/MapsActivity.java
+++ app/src/main/java/aarhusuniversitet/brightcycle/MapsActivity.java
@@ -1,20 +1,20 @@
 package aarhusuniversitet.brightcycle;
 
 import android.Manifest;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
@@ -27,16 +27,17 @@ import timber.log.Timber;
 
 import static aarhusuniversitet.brightcycle.R.id.map;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationProvider.LocationCallback {
 
     @BindView(R.id.content_main)
     RelativeLayout contentMain;
 
     private GoogleMap mMap;
+    private LocationProvider mLocationProvider;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +56,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         getSupportActionBar().setHomeButtonEnabled(false);
 
         createAppDrawer();
-    }
 
+        mLocationProvider = new LocationProvider(this, this);
+    }
 
     /**
      * Manipulates the map once available.
@@ -80,6 +82,53 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mLocationProvider.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
+    
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            SupportMapFragment mMap = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(map);
+            mMap.getMapAsync(this);
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    public void handleNewLocation(Location location) {
+        Timber.d(location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
 
     public void createAppDrawer() {
         //if you want to update the items at a later time it is recommended to keep it in a variable
--- app/src/main/java/aarhusuniversitet/brightcycle/Utility/Utility.java
+++ app/src/main/java/aarhusuniversitet/brightcycle/Utility/Utility.java
@@ -1,8 +1,19 @@
 package aarhusuniversitet.brightcycle.Utility;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import aarhusuniversitet.brightcycle.MapsActivity;
+
 /**
  * Created by janek on 17/11/2016.
  */
 
 public class Utility {
+    public static boolean checkLocationPermission(Context view) {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = view.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
 }
